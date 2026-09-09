dd074b6f67163d6723d1c5a943b0c54ecd4fa8c0
Babsinogen <babsinogen@users.noreply.github.com>
2026-09-09 07:08:55 +0000

    New Babsinogen mark: terracotta nest-and-pearl icon
    Replaces the concentric-arc logo with a warmer, higher-contrast caliper/nest mark on a terracotta tile. PWA cache v7.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "babsinogen-v6";
+const CACHE_NAME = "babsinogen-v7";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./logo-mark.png", "./favicon.svg"];
 
 self.addEventListener("install", (event) => {
