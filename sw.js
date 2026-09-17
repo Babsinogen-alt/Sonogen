e5c3fef5688f33dbcf7d413a69fb343c666be3ab
Babsinogen <babsinogen@users.noreply.github.com>
2026-09-17 19:33:22 +0000

    Expand clinical quiz to 320 sourced scenarios (8×40).

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "babsinogen-v15";
+const CACHE_NAME = "babsinogen-v16";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./logo-mark.png", "./favicon.svg", "./apple-touch.png"];
 
 self.addEventListener("install", (event) => {
