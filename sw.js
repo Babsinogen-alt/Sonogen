88ac5ac7a4fce3a84e18576d622a0f5a5b2d82c6
Babsinogen <babsinogen@users.noreply.github.com>
2026-09-22 21:18:16 +0000

    Add sourced red-flag atlas (ectopic, Doppler, vasa previa, PAS) on Protocols.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "babsinogen-v17";
+const CACHE_NAME = "babsinogen-v20";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./logo-mark.png", "./favicon.svg", "./apple-touch.png"];
 
 self.addEventListener("install", (event) => {
